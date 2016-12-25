5346b02c793f344292c7afbca84bc2bd65e83dbb
Alexander <jonston1@rambler.ru>
2016-12-25 17:17:10 +0300

    Stable version v1.0

---
+++ js/app/directives/contentEditor.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Александр on 25.12.2016.
+ */
+
+homeApp.directive("content-editor", function () {
+    return function (scope, element, attrs) {
+
+        element.css("background", "red");
+
+    }
+});
